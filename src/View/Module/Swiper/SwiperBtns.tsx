771a8c9c312ab7261b9be142064f0884aa2bec4b
Kostya Omelchenko <kostiaomelcenko@gmail.com>
2023-05-28 15:22:10 +0200

    swiper styles

--- src/View/Module/Swiper/SwiperBtns.tsx
+++ src/View/Module/Swiper/SwiperBtns.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from "react";
 import { range } from 'ramda';
 import './swiper.scss';
+import classNames from "classnames";
 
 interface Props {
     changeSlideAction: (index: number) => any
@@ -9,17 +10,34 @@ interface Props {
 }
 
 export const SwiperBtns: React.FC<Props> = ({ changeSlideAction, elemesCount, activeIndex }) => {
+    const leftPadding = activeIndex * 22;
+
     return useMemo(() => (
         <div className="swiper-btns">
             {
                 range(0, elemesCount).map((i) => (
                     <button 
                         key={i + 1}
-                        onClick={() => changeSlideAction(i)}>
-                            {i + 1}
+                        onClick={() => changeSlideAction(i)}
+                        // className={classNames(
+                        //     {
+                        //         "swiper-btn-active": i === activeIndex
+                        //     }
+                        // )}
+                    >
                     </button>
                 ))
             }
+            <button
+              className="activator-btn"
+              style={{
+                position: 'absolute',
+                top: '1.75px',
+                left: `${leftPadding}px`
+              }} 
+            >
+
+            </button>
         </div>
     ), [activeIndex])
 }
